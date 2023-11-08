664932d9eef8630f9503b6c28658057150f54768
Krasyo <doanusho@gmail.com>
2023-11-08 18:40:05 +0000

    add 2nd part of 2nd day 2022 challenge

--- 2022/day2RockPaperScissors.js
+++ 2022/day2RockPaperScissors.js
@@ -22,6 +22,18 @@ In the first round, your opponent will choose Rock (A), and you should choose Pa
 In the second round, your opponent will choose Paper (B), and you should choose Rock (X). This ends in a loss for you with a score of 1 (1 + 0).
 The third round is a draw with both players choosing Scissors, giving you a score of 3 + 3 = 6.
 In this example, if you were to follow the strategy guide, you would get a total score of 15 (8 + 1 + 6).
+
+--- Part Two ---
+The Elf finishes helping with the tent and sneaks back over to you. "Anyway, the second column says how the round needs to end: X means you need to lose, Y means you need to end the round in a draw, and Z means you need to win. Good luck!"
+
+The total score is still calculated in the same way, but now you need to figure out what shape to choose so the round ends as indicated. The example above now goes like this:
+
+In the first round, your opponent will choose Rock (A), and you need the round to end in a draw (Y), so you also choose Rock. This gives you a score of 1 + 3 = 4.
+In the second round, your opponent will choose Paper (B), and you choose Rock so you lose (X) with a score of 1 + 0 = 1.
+In the third round, you will defeat your opponent's Scissors with Rock for a score of 1 + 6 = 7.
+Now that you're correctly decrypting the ultra top secret strategy guide, you would get a total score of 12.
+
+Following the Elf's instructions for the second column, what would your total score be if everything goes exactly according to your strategy guide?
 */
 
 const fs = require("fs/promises")
@@ -39,15 +51,8 @@ const getPlays = async () => {
 }
 
 const calculateScore = (arr) => {
-  const playVals = {"X": 1, "Y": 2, "Z": 3 }
-  const winners = {
-    "X": "C",
-    "Y": "A",
-    "Z": "B",
-    "A": "Z",
-    "B": "X",
-    "C": "Y"
-  }
+  const playVals = { "X": 1, "Y": 2, "Z": 3 }
+  const winners = { "X": "C", "Y": "A", "Z": "B", "A": "Z", "B": "X", "C": "Y" }
 
 
   if(winners[arr[1]] === arr[0]) {
@@ -59,12 +64,27 @@ const calculateScore = (arr) => {
   }
 }
 
+const calculateNewScore = (arr) => {
+  const playVals = {"A":1, "B":2, "C":3 ,"X": 1, "Y": 2, "Z": 3 }
+  const lose = { "A": "Z", "B": "X", "C": "Y" }
+  const win = { "A": "Y", "B": "Z", "C": "X" }
+
+  if(arr[1] === "X") {
+    return playVals[lose[arr[0]]]
+  } else if (arr[1] === "Y") {
+    return playVals[arr[0]] + 3
+  } else {
+    return playVals[win[arr[0]]] + 6
+  }
+
+}
+
 const calculateFinalScore = async () => {
   const arrOfPlays = await getPlays()
 
   let score = 0
   for (let i = 0; i < arrOfPlays.length; i++) {
-    const currPlayScore = calculateScore(arrOfPlays[i])
+    const currPlayScore = calculateNewScore(arrOfPlays[i])
     score += currPlayScore
   }
 
